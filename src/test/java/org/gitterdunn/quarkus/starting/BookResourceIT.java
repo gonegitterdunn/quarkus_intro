f533537b4b12843b7f537e590cee19b7fd522fa8
Gitter Dunn <gitterdunn@protonmail.com>
2023-03-07 20:32:51 +0100

    running native and native tests

--- src/test/java/org/gitterdunn/quarkus/starting/BookResourceIT.java
+++ src/test/java/org/gitterdunn/quarkus/starting/BookResourceIT.java
@@ -1,8 +1,29 @@
 package org.gitterdunn.quarkus.starting;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import org.junit.jupiter.api.Test;
 
 @QuarkusIntegrationTest
 public class BookResourceIT extends BookResourceTest {
-    // Execute the same tests but in packaged mode.
+  // Execute the same tests but in packaged mode.
+  @Test
+  public void testGetSpecificBook() {
+    given()
+        .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+        .pathParams("id", 1)
+        .when()
+        .get("api/books/{id}")
+        .then()
+        .statusCode(200)
+        .body("id", is(1))
+        .body("title", is("Understanding Quarkus"))
+        .body("author", is("Antonio"))
+        .body("yearOfPublication", is(2020))
+        .body("genre", is("Information Technology"));
+  }
 }
--- src/test/java/org/gitterdunn/quarkus/starting/BookResourceTest.java
+++ src/test/java/org/gitterdunn/quarkus/starting/BookResourceTest.java
@@ -1,53 +1,51 @@
 package org.gitterdunn.quarkus.starting;
 
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-
-import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.MediaType;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
+
+import io.quarkus.test.junit.QuarkusTest;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import org.junit.jupiter.api.Test;
 
 @QuarkusTest
 public class BookResourceTest {
 
-    @Test
-    public void testGetAllBooks() {
-        given()
-                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
-                .when()
-                .get("/api/books")
-                .then()
-                .statusCode(200)
-                .body("size()", is(4));
-    }
+  @Test
+  public void testGetAllBooks() {
+    given()
+        .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+        .when()
+        .get("/api/books")
+        .then()
+        .statusCode(200)
+        .body("size()", is(4));
+  }
 
-    @Test
-    public void testCountAllBooks() {
-        given()
-                .header(HttpHeaders.ACCEPT, MediaType.TEXT_PLAIN)
-                .when()
-                .get("/api/books/count")
-                .then()
-                .statusCode(200)
-                .body(is("4"));
-    }
+  @Test
+  public void testCountAllBooks() {
+    given()
+        .header(HttpHeaders.ACCEPT, MediaType.TEXT_PLAIN)
+        .when()
+        .get("/api/books/count")
+        .then()
+        .statusCode(200)
+        .body(is("4"));
+  }
 
-    @Test
-    public void testGetSpecificBook() {
-        given()
-                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
-                .pathParams("id", 1)
-                .when()
-                .get("api/books/{id}")
-                .then()
-                .statusCode(200)
-                .body("id", is(1))
-                .body("title", is("Understanding Quarkus"))
-                .body("author", is("Antonio"))
-                .body("yearOfPublication", is(2020))
-                .body("genre", is("IT"));
-    }
-
+  @Test
+  public void testGetSpecificBook() {
+    given()
+        .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+        .pathParams("id", 1)
+        .when()
+        .get("api/books/{id}")
+        .then()
+        .statusCode(200)
+        .body("id", is(1))
+        .body("title", is("Understanding Quarkus"))
+        .body("author", is("Antonio"))
+        .body("yearOfPublication", is(2020))
+        .body("genre", is("Comedy"));
+  }
 }
